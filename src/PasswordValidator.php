059ef775cc82fc63265c7c2ae9c9f2061a7163e0
Adam Averay <adam@averay.com>
2026-05-27 14:52:09 +0100

    Mark overridden methods

--- src/PasswordValidator.php
+++ src/PasswordValidator.php
@@ -12,11 +12,13 @@ final readonly class PasswordValidator implements PasswordValidatorInterface
    */
   public function __construct(private string $algorithm = \PASSWORD_DEFAULT, private array $options = []) {}
 
+  #[\Override]
   public function hash(#[\SensitiveParameter] string $cleartext): string
   {
     return password_hash($cleartext, $this->algorithm, $this->options);
   }
 
+  #[\Override]
   public function verify(#[\SensitiveParameter] string $cleartext, #[\SensitiveParameter] string $hash): bool
   {
     return password_verify($cleartext, $hash);
